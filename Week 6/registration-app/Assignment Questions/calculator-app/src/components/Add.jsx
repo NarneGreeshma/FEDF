587b5684738cbe84f60ffd33882d423d6aff0395
NarneGreeshma <narnegreeshma14@gmail.com>
2026-06-08 08:51:38 +0530

    Update week 6 registration app

--- Week 6/registration-app/Assignment Questions/calculator-app/src/components/Add.jsx
+++
@@ -1,9 +0,0 @@
-function Add({ num1, num2, setResult }) {
-  return (
-    <button onClick={() => setResult(Number(num1) + Number(num2))}>
-      Add
-    </button>
-  );
-}
-
-export default Add;
--- Week 6/registration-app/Assignment Questions/calculator-app/src/components/Multiply.jsx
+++
@@ -1,9 +0,0 @@
-function Multiply({ num1, num2, setResult }) {
-  return (
-    <button onClick={() => setResult(Number(num1) * Number(num2))}>
-      Multiply
-    </button>
-  );
-}
-
-export default Multiply;
