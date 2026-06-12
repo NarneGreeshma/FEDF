b58f0fa7ff8b2a3e37b0dc1d85b9bcbc17e36168
NarneGreeshma <narnegreeshma14@gmail.com>
2026-06-12 09:38:19 +0530

    file created

---
+++ Week 12/Week 12/src/components/BookingForm.jsx
@@ -0,0 +1,13 @@
+function BookingForm() {
+  return (
+    <div>
+      <h2>Flight Booking</h2>
+
+      <button>
+        Book Ticket
+      </button>
+    </div>
+  );
+}
+
+export default BookingForm;
